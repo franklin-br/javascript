bfe4d10dfa6efe8b1ec39a7c4fd9afda5bb3e46f
dev-franklin <91273045+dev-franklin@users.noreply.github.com>
2022-02-08 23:58:43 -0300

    Initial commit

---
+++ desafios/testeif/test.js
@@ -0,0 +1,7 @@
+var país = "China"
+console.log(`Vivendo em ${país}`)
+if (país == `Brasil`){
+    console.log(`Você é BR`)
+} else {
+    console.log(`Você é estrangeiro`)
+}
---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`Á velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){  //Condição Simples!
+    console.log(`Você ultrapassou o limite de velocidade, MULTADO!`)
+}else //Condiçao Composta!
+console.log(`Dirija sempre usano sinto de segurança!`)
